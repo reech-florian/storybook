61d600346cc6d6b97a31d35f9735d1b59643b104
Kyle Gach <kyle.gach@gmail.com>
2022-08-16 21:55:41 -0600

    Add `nextjs` framework

---
+++ code/frameworks/nextjs/src/routing/webpack.tsx
@@ -0,0 +1,24 @@
+import { Configuration as WebpackConfig } from 'webpack';
+import semver from 'semver';
+import { addScopedAlias, getNextjsVersion } from '../utils';
+
+export const configureRouting = (baseConfig: WebpackConfig): void => {
+  // here we resolve the router context path with the installed version of Next.js
+  const routerContextPath = getRouterContextPath();
+  addScopedAlias(baseConfig, routerContextPath);
+  addScopedAlias(
+    baseConfig,
+    // TODO: ESBuild says this module needs to be marked as external
+    require.resolve('./resolved-router-context'),
+    routerContextPath
+  );
+};
+
+const getRouterContextPath = () => {
+  const version = getNextjsVersion();
+  if (semver.gte(version, '11.1.0')) {
+    return 'next/dist/shared/lib/router-context';
+  }
+
+  return 'next/dist/next-server/lib/router-context';
+};
